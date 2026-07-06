f16638ea45e3e6f5b346620acb56e17e33365580
angel1331 <rakashev39@gmail.com>
2026-07-06 20:10:07 +0500

    Added 2 methods for load clients binary.

--- src/model/Person.java
+++ src/model/Person.java
@@ -2,7 +2,9 @@ package model;
 
 import exceptions.InvalidPhoneException;
 
-public abstract class Person {
+import java.io.Serializable;
+
+public abstract class Person implements Serializable {
     private String name;
     private String phone;
 
